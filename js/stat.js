66afa080519c4295d462e15bf3c70dcdd3fb8c5e
ElenaKsirova <30499327+ElenaKsirova@users.noreply.github.com>
2017-08-16 12:55:45 +0300

    Разбила функцию renderStatistics на мелкие функции

--- js/stat.js
+++ js/stat.js
@@ -1,93 +1,124 @@
 'use strict';
 
 function renderStatistics(ctx, names, times) {
-  var WIN_WIDTH = 420;
-  var WIN_HEIGHT = 270;
+  function getMaxOfArray(arr) {
+    var maxValue = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] > maxValue) {
+        maxValue = arr[i];
+      }
+    }
+
+    return maxValue;
+  }
+
+
   var WIN_LEFT = 100;
   var WIN_TOP = 10;
+  var WIN_WIDTH = 420;
+  var WIN_HEIGHT = 270;
 
   var WIN_COLOR = 'white';
 
   var WIN_SHADOW_OFFSET = 10;
   var WIN_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 
-  var MSG_PADDING_LEFT = 45;
-  var MSG_PADDING_TOP = 20;
-
   var COL_HEIGHT_MAX = 150;
   var COL_WIDTH = 40;
   var COL_PADDING = 50;
   var COL_BOTTOM = 240;
 
-  // ограничиваем снизу прозрачность столбца,
-  // чтобы в статистике не получался прозрачный столбец
-  var COL_MIN_ALPHA = 0.3;
+  var COL_CAPTIONS_COLOR = 'black';
 
-  var MY_COL_NAME = 'Вы';
-  var MY_COL_COLOR = 'rgba(255, 0, 0, 1)';
+  var FIRST_COL_LEFT = 140;
 
-  var colLeft = 140;
 
-  var colHeight;
-  var colAlpha;
+  var maxTime = getMaxOfArray(times);
 
-  var heightScale;
-
-  var maxTime = 0;
+  // вычисляем коэффициент масштабирования для столбцов, с защитой от деления на ноль
+  var heightScale = (maxTime ? (COL_HEIGHT_MAX / maxTime) : COL_HEIGHT_MAX);
 
   var playerIndex;
 
 
-  // рисуем сначала тень от окна, т.к. она должна лежать под окном
+  var drawWindowShadow = function () {
+    ctx.fillStyle = WIN_SHADOW_COLOR;
 
-  ctx.fillStyle = WIN_SHADOW_COLOR;
-  ctx.fillRect(WIN_LEFT + WIN_SHADOW_OFFSET, WIN_TOP + WIN_SHADOW_OFFSET, WIN_WIDTH, WIN_HEIGHT);
-
-  // затем рисуем окно
-
-  ctx.fillStyle = WIN_COLOR;
-  ctx.fillRect(WIN_LEFT, WIN_TOP, WIN_WIDTH, WIN_HEIGHT);
-
-  ctx.font = '16px PT Mono';
-  ctx.strokeStyle = 'black';
-
-  ctx.strokeText('Ура вы победили!', WIN_LEFT + MSG_PADDING_LEFT, WIN_TOP + MSG_PADDING_TOP);
-  ctx.strokeText('Список результатов:', WIN_LEFT + MSG_PADDING_LEFT, WIN_TOP + MSG_PADDING_TOP + 20);
+    ctx.fillRect(
+        WIN_LEFT + WIN_SHADOW_OFFSET, WIN_TOP + WIN_SHADOW_OFFSET,
+        WIN_WIDTH, WIN_HEIGHT
+    );
+  };
 
 
-  // вычисляем максимальный результат (время) в статистике
-
-  for (playerIndex = 0; playerIndex < times.length; playerIndex++) {
-    if (times[playerIndex] > maxTime) {
-      maxTime = times[playerIndex];
-    }
-  }
+  var drawWindow = function () {
+    ctx.fillStyle = WIN_COLOR;
+    ctx.fillRect(WIN_LEFT, WIN_TOP, WIN_WIDTH, WIN_HEIGHT);
+  };
 
 
-  // вычисляем коэффициент масштабирования для столбцов,
-  // с защитой от деления на ноль
+  var drawWindowCaption = function () {
+    var MSG_PADDING_LEFT = 45;
+    var MSG_PADDING_TOP = 20;
 
-  heightScale = (maxTime ? (COL_HEIGHT_MAX / maxTime) : COL_HEIGHT_MAX);
+    ctx.font = '16px PT Mono';
+    ctx.strokeStyle = 'black';
 
-  for (playerIndex = 0; playerIndex < names.length; playerIndex++) {
-    if (names[playerIndex] === MY_COL_NAME) {
-      ctx.fillStyle = MY_COL_COLOR;
-    }
-    else {
-      colAlpha = Math.random() * (1 - COL_MIN_ALPHA) + COL_MIN_ALPHA;
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + colAlpha + ')';
-    }
+    ctx.strokeText('Ура вы победили!', WIN_LEFT + MSG_PADDING_LEFT, WIN_TOP + MSG_PADDING_TOP);
+    ctx.strokeText('Список результатов:', WIN_LEFT + MSG_PADDING_LEFT, WIN_TOP + MSG_PADDING_TOP + 20);
+  };
 
-    colHeight = times[playerIndex] * heightScale;
+
+  var getRandomColor = function () {
+    // ограничиваем снизу прозрачность, чтобы избежать 100%-ной прозрачности
+    var MIN_ALPHA = 0.3;
+    var alpha = Math.random() * (1 - MIN_ALPHA) + MIN_ALPHA;
+
+    return ('rgba(0, 0, 255, ' + alpha + ')');
+  };
+
+
+  var getColumnColor = function (index) {
+    var MY_COL_NAME = 'Вы';
+    var MY_COL_COLOR = 'rgba(255, 0, 0, 1)';
+
+    return ((names[index] === MY_COL_NAME) ? MY_COL_COLOR : getRandomColor());
+  };
+
+
+  var drawColumn = function (index, color) {
+    var colLeft = FIRST_COL_LEFT + (index * (COL_WIDTH + COL_PADDING));
+    var colHeight = times[index] * heightScale;
+
+    ctx.fillStyle = color;
 
     ctx.fillRect(colLeft, COL_BOTTOM - colHeight, COL_WIDTH, colHeight);
+  };
+
+
+  var drawColumnCaptions = function (index) {
+    var textLeft = FIRST_COL_LEFT + (index * (COL_WIDTH + COL_PADDING)) + 1;
+    var colHeight = times[index] * heightScale;
 
     // снизу столбца пишем имя очередного игрока
-    ctx.strokeText(names[playerIndex], colLeft + 1, COL_BOTTOM + 20);
+    ctx.strokeText(names[index], textLeft, COL_BOTTOM + 20);
 
     // над столбцом пишем его результат
-    ctx.strokeText(Math.floor(times[playerIndex]), colLeft + 1, COL_BOTTOM - colHeight - 10);
+    ctx.strokeText(Math.floor(times[index]), textLeft, COL_BOTTOM - colHeight - 10);
+  };
 
-    colLeft += (COL_WIDTH + COL_PADDING);
+
+  drawWindowShadow();
+
+  drawWindow();
+
+  drawWindowCaption();
+
+
+  for (playerIndex = 0; playerIndex < names.length; playerIndex++) {
+    drawColumn(playerIndex, getColumnColor(playerIndex));
+
+    drawColumnCaptions(playerIndex, COL_CAPTIONS_COLOR);
   }
 }
