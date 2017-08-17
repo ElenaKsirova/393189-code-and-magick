bc8bc85fb9ded542741939574da2879dbafb6dfd
ElenaKsirova <30499327+ElenaKsirova@users.noreply.github.com>
2017-08-17 22:01:36 +0300

    Выполнила задание #8 Учебный проект: нас орда

---
+++ js/setup.js
@@ -0,0 +1,89 @@
+'use strict';
+
+function getRandomItem(arrOfItems) {
+  return arrOfItems[Math.floor(Math.random() * arrOfItems.length)];
+}
+
+var WIZARD_FIRST_NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+
+var WIZARD_LAST_NAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+
+var WIZARD_COAT_COLORS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
+var WIZARD_EYES_COLORS = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
+
+var createWizards = function (count) {
+  var w = [];
+
+  for (var i = 0; i < count; i++) {
+    w[i] = {
+      name: getRandomItem(WIZARD_FIRST_NAMES) + ' ' + getRandomItem(WIZARD_LAST_NAMES),
+      coatColor: getRandomItem(WIZARD_COAT_COLORS),
+      eyesColor: getRandomItem(WIZARD_EYES_COLORS)
+    };
+  }
+
+  return w;
+};
+
+
+var WIZARD_COUNT = 4;
+
+var wizards = createWizards(WIZARD_COUNT);
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
